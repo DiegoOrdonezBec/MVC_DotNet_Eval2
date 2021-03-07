7f4079752c0bd4935054c18ae57057a66de6cbad
Diego Ordoñez <edexgamer@gmail.com>
2021-03-07 14:20:50 -0600

    Agregar archivos de proyecto.

---
+++ Diego_Ordonez_18300218_MVC_Eval_2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Diego_Ordonez_18300218_MVC_Eval_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diego_Ordonez_18300218_MVC_Eval_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Diego_Ordonez_18300218_MVC_Eval_2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Diego_Ordonez_18300218_MVC_Eval_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
